c1dab05abda80e17efe3ab3b2294a1f888f6e932
giacinto90 <giacinto.am@gmail.com>
2020-09-02 13:34:52 +0100

    fixed movement of all viruses

--- scripts/app.js
+++ scripts/app.js
@@ -9,41 +9,56 @@ function init() {
   const gridCells = width * width
   let virusPosition = null
   let vaccinePosition = 62
-  let allViruses = []
+  // let allViruses = [0, 1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 24, 25, 26]
   let playerPosition = 94
   let shotPosition = playerPosition
+  let sneezePosition = virusPosition
   let timerShoot = null
   let allShots = []
-  // let shot = null
+  let virusId
+  let allViruses = [
+    { currentIndex: 0, isAlive: true },
+    { currentIndex: 1, isAlive: true },
+    { currentIndex: 2, isAlive: true },
+    { currentIndex: 3, isAlive: true },
+    { currentIndex: 4, isAlive: true }
+  ]
 
   //* Scenario
   function createGrid() {
     for (let i = 0; i < gridCells; i++) {
       const cell = document.createElement('div')
       cell.setAttribute('id', i)
+      cell.innerHTML = i
       cells.push(cell)
       grid.appendChild(cell)
     }
-    // createMasks()
-    masksDom()
+    createMasks()
+    // masksDom()
   }
   createGrid()
 
-  // function createMasks() {
-  //   maskPosition.forEach(mask => {
-  //     return cells[mask].classList.add('mask')
-  //   })
-  // }
-
-  function masksDom() {
-    const firstMask = document.getElementById('71')
-    const secondMask = document.getElementById('72')
-    const thirdMask = document.getElementById('74')
-    const fourthMask = document.getElementById('75')
-    const fifthMask = document.getElementById('77')
-    const sixthMask = document.getElementById('78')
+  function createMasks() {
+    maskPosition.forEach(mask => {
+      return cells[mask].classList.add('mask')
+    })
   }
-
+  
+  // function masksDom() {
+  //   const firstMask = document.getElementById('71')
+  //   const secondMask = document.getElementById('72')
+  //   const thirdMask = document.getElementById('74')
+  //   const fourthMask = document.getElementById('75')
+  //   const fifthMask = document.getElementById('77')
+  //   const sixthMask = document.getElementById('78')
+  //   const firstPair = firstMask + secondMask
+  //   const secondPair = thirdMask + fourthMask
+  //   const thirdPair = fifthMask + sixthMask
+  //   cells[firstPair].classList.add('mask')
+  //   cells[secondPair].classList.add('mask')
+  //   cells[thirdPair].classList.add('mask')
+  // }
+  
   //* Viruses Spawns, Movement and Sneeze
   // function addVaccine(vaccinePosition) {
   //   cells[vaccinePosition].classList.add('vaccine')
@@ -63,45 +78,70 @@ function init() {
   // addViruses(virusPosition)
 
   function addViruses() {
-    for (let i = 0; i < 28; i++) {
-      allViruses.push(i)
-      allViruses.forEach(virus => {
-        return cells[virus].classList.add('virus')
+    allViruses.forEach(virus => {
+      if (virus.isAlive) {
+        return cells[virus.currentIndex].classList.add('virus')
+      }
+    })
+  }
+  addViruses()
+
+  function moveViruses() {
+    const finalVirus = allViruses[allViruses.length - 1].currentIndex
+    const x = finalVirus % width
+    const y = Math.floor(finalVirus / width)
+    if (x < width - 1 && y % 2 === 0) {
+      allViruses = allViruses.map(virus => {
+        return {
+          ...virus, currentIndex: virus.currentIndex + 1
+        }
+      })
+    } else if (x > allViruses.length - 1 && y % 2 !== 0) {
+      allViruses = allViruses.map(virus => {
+        return {
+          ...virus, currentIndex: virus.currentIndex - 1
+        }
+      })
+    } else if (x === width - 1 || x === allViruses.length - 1) {
+      allViruses = allViruses.map(virus => {
+        return {
+          ...virus, currentIndex: virus.currentIndex + width
+        }
       })
     }
   }
-  addViruses()
   
-  
-
-  function moveViruses() {
+  function move() {
     let count = 0
-    let timerId = setInterval(() => {
-      removeVirus()
-      allViruses = allViruses.map(virus => {
-        return virus + 1
-      })
+    virusId = setInterval(() => {
+      removeAllViruses()
+      moveViruses()
       addViruses()
     }, 1000)
   }
-  // moveViruses()
+  move()
   
   
-  function sneeze(virusPosition) {
-    
-  }
+  // function sneeze() {
+  //   addSneeze()
+  //   sneezePosition + 30
+  // }
+  // sneeze()
+
+  // function addSneeze(sneezePosition) {
+  //   cells[sneezePosition].classList.add('sneeze')
+  // }
 
   //* Remove Functions
   function removeVirus() {
     cells[shotPosition].classList.remove('virus')
   }
 
-  // function removeAllViruses() {
-  //   for (let i = 0; i < 28; i++) {
-  //     cells[virusPosition].classList.remove('virus')
-  //     console.log(i)
-  //   }
-  // }
+  function removeAllViruses() {
+    allViruses.forEach(virus => {
+      cells[virus.currentIndex].classList.remove('virus')
+    })
+  }
   
   function removePlayer(playerPosition) {
     cells[playerPosition].classList.remove('player') 
@@ -113,6 +153,10 @@ function init() {
     cells[shotPosition].classList.remove('shoot')
   }
 
+  function removeSneeze() {
+    cells[sneezePosition].classList.remove('sneeze') 
+  }
+
   //* Player Classes
   function addPlayer(playerPosition) {
     cells[playerPosition].classList.add('player')
@@ -122,13 +166,21 @@ function init() {
   //* Shoot Functions
   function handleShoot() {
     if (cells[shotPosition].classList.contains('virus')) {
+      const hitVirus = allViruses.find(virus => {
+        virus.currentIndex === shotPosition
+      })
+      console.log(hitVirus, 'hitVirus')
+      hitVirus.isAlive = !hitVirus.isAlive
+      console.log(allViruses, 'all viruses')
+      console.log(shotPosition, 'shot')
       removeShoot()
       removeVirus()
       clearInterval(timerShoot)
-    } else if (shotPosition % width === 0) {
-      removeShoot()
-      clearInterval(timerShoot)
-    }
+    } 
+    // else if (shotPosition % width === 0) {
+    //   removeShoot()
+    //   clearInterval(timerShoot)
+    // }
     // allShots.forEach(shot => {
     //   allShots.push(shotPosition)
     //   console.log(allShots)
